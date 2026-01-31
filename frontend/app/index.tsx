5343674076f5631985902dd99157cb7a405ed1ba
alex-bo7 <anino882@gmail.com>
2026-01-30 18:11:06 -0800

    Added expo router

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
+import { router } from "expo-router"
 
 export default function Index() {
   return (
@@ -10,6 +11,12 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+
+      {/* temp routing */}
+      <Button
+        title="Go to Map page"
+        onPress={() => router.push("/map")}
+      />
     </View>
   );
 }
