2c869d64eeb27bb7bffdf9189b3fbe0183fdced1
alex-bo7 <anino882@gmail.com>
2026-02-06 12:03:28 -0800

    Button to update location manually

--- frontend/app/map.tsx
+++ frontend/app/map.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from "react";
-import { View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { StyleSheet, View, Text, Pressable } from "react-native";
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 
 export default function Map() {
     const [location, setLocation] = useState<Location.LocationObject | null>(null);
     const [errorMsg, setErrorMsg] = useState<string | null>(null);
+    const mapRef = useRef<MapView | null>(null);
     
     useEffect(() => {
         async function getCurrentLocation() {
@@ -23,34 +24,75 @@ export default function Map() {
         getCurrentLocation();
     }, []);
 
-    let locationText = 'Waiting...';
-    let userLatitude = 0
-    let userLongitude = 0
-    if (errorMsg) {
-        locationText = errorMsg;
-    } else if (location) {
-        userLatitude = location!.coords.latitude
-        userLongitude = location!.coords.longitude
+    async function updateLocation() {
+        const loc = await Location.getCurrentPositionAsync({});
+        setLocation(loc);
     }
 
+    if (errorMsg) {
+        return (
+            <View style={styles.page}>
+                <Text>{errorMsg}</Text>
+            </View>
+        );
+    }
+
+    if (!location) {
+        return (
+            <View style={styles.page}>
+                <Text>Waiting...</Text>
+            </View>
+        );
+    }
+
+    const { latitude, longitude } = location.coords;
+
     return (
-        <View style={{flex: 1}}>
+        <View style={styles.page}>
 
             <MapView
-            style={{width: "100%", height: "100%"}}
+            style={styles.map}
             initialRegion={{
-                latitude: userLatitude,
-                longitude: userLongitude,
+                latitude: latitude,
+                longitude: longitude,
                 latitudeDelta: 0.01,
                 longitudeDelta: 0.01,
             }}>
 
                 <Marker
-                coordinate={{ latitude: userLatitude, longitude: userLongitude }}
+                coordinate={{ latitude: latitude, longitude: longitude }}
                 title="User" // change to name or account type?
                 />
             </MapView>
 
+            <Pressable
+            onPress={updateLocation}
+            style={({ pressed }) => [
+                styles.btn,
+                pressed && styles.btnPressed,
+            ]}>
+                <Text style={{textAlign: 'center'}}>Update Location</Text>
+            </Pressable>
+
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+        paddingHorizontal: 15,
+    },
+    map: {
+        width: "100%",
+        height: "75%",
+        paddingVertical: 15,
+    },
+    btn: {
+        paddingVertical: 25,
+        backgroundColor: 'grey',
+    },
+    btnPressed: {
+        opacity: 0.5
+    }
+})
