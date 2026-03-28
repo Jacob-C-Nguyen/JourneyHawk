c42454e48d64d9c0dca8b7e9b789f7b0a4dd64d9
Tn <asisduous@gmail.com>
2026-03-27 18:43:04 -0700

    organizing

--- src/utils/generateRoomCode.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-const generateRoomCode = () => {
-  // Generate 8-character random code (e.g., "A3F9B2C1")
-  return crypto.randomBytes(4).toString('hex').toUpperCase();
-};
-
-module.exports = generateRoomCode;
